db703c3ce352e2adc576436014ff21c807c03bba
Yann-Wei <weileryann3@gmail.com>
2024-09-04 14:42:45 +0200

    Update website.py

--- website.py
+++ website.py
@@ -6,4 +6,9 @@ st.subheader("This is a subtitle")
 st.title("This is a title")
 st.write("Here is where you write")
 st.write("[Learn More> ](https://docs.streamlit.io/develop/quick-reference/cheat-sheet)")
-st.button("Click me")
+count=0
+if st.button("Click me"):
+  count+=1
+  st.write(count)
+  
+  
