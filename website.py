75e86ed74a0b63bbf194e9765bc087a75a227d27
Yann-Wei <weileryann3@gmail.com>
2024-09-04 14:53:45 +0200

    Update website.py

--- website.py
+++ website.py
@@ -7,7 +7,7 @@ st.title("This is a title")
 st.write("Here is where you write")
 st.write("[Learn More> ](https://docs.streamlit.io/develop/quick-reference/cheat-sheet)")
 if 	'count' not in st.session_state:
-  st.session_state.count = 0
+  st.session_state.count = ""
 
 if st.button("Click me"):
   st.session_state.count +=1
