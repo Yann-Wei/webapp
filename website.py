38994736fb39dabb3bf0a04f0dc09002e11f0183
Yann-Wei <weileryann3@gmail.com>
2024-09-04 14:28:08 +0200

    Update website.py

--- website.py
+++ website.py
@@ -1,2 +1,8 @@
 import streamlit as st
-st.set_page_config(page_title="Hello Webpage")
+st.set_page_config(page_title="Hello Webpage", layout= "wide")
+
+#header
+st.subheader("This is a subtitle")
+st.title("This is a title")
+st.write("Here is where you write")
+st.write("[Learn More> ](https://docs.streamlit.io/develop/quick-reference/cheat-sheet)")
