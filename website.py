75bedad238d01d47b2ba6b53459d8ebfeee206c1
Yann-Wei <weileryann3@gmail.com>
2024-09-04 21:41:16 +0200

    Update website.py

--- website.py
+++ website.py
@@ -19,7 +19,7 @@ html_content= """
 <h1> This is a title </h1>
 <p> This is my first website </p>
 
-<img src= https://lens.google.com/search?ep=gsbubb&hl=en-LU&re=df&p=AbrfA8os4HOF3mUmr_WEsvj3etKj4FUN-jb5eyBMR7jZbVFRR-ssKFcK8to9AEIh2LXkjKW8bR_XLZmdGJ-dNB9OWdJfiu08923oGT_kVSN01rft6CCvqYfoYFXD_LN4KRz_E-Q8iqmy_chDPx248TkFapQQJs05wuJLsbkjuMhJn7e1QkFVMtGlRIbNmDPjfl_GNiEntotXMETQew%3D%3D#lns=W251bGwsbnVsbCxudWxsLG51bGwsbnVsbCxudWxsLG51bGwsIkVrY0tKR1UwWldRME1qWTNMVFE1T0dJdE5HTTFOUzA1WmpZMExUWmxObUl6TURFMVpqbGtPUklmU1RSVkxVOVFkRTlhYkdObU5FWXpjRVF5ZGpoaFkzUmxNVmxmYjBkNGF3PT0iLG51bGwsbnVsbCxudWxsLG51bGwsbnVsbCxudWxsLG51bGwsWyI2ODBlNDZlYS0yODNkLTQzODQtYjNhMi02OWE4OTQ0ZWViYWUiXV0=/>
+<img src= https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.flaticon.com%2Ffree-icon%2Fnotepad_1584889&psig=AOvVaw08OT-VIMrFPKSMCMiboqs1&ust=1725564872581000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCMC39qiFqogDFQAAAAAdAAAAABAE/>
 """
 st.markdown(html_content, unsafe_allow_html =True)
 
